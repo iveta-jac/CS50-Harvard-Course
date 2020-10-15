8e643508214ae7e213113b0d263c514edd04206d
pocketful <iveta.jac@gmail.com>
2020-10-15 18:02:20 +0300

    open data file

--- pset7-2-houses/import.py
+++ pset7-2-houses/import.py
@@ -1,1 +1,24 @@
-# TODO
+from cs50 import SQL
+from sys import argv, exit
+import csv
+
+
+# Set up a database connection
+db = SQL("sqlite:///students.db")
+
+def main():
+
+    # Check for correct number of args
+    if len(argv) != 2:
+        print("Usage: python import.py characters.csv")
+        exit(1)
+
+    # Open data file
+    with open(argv[1], 'r') as csv_file:
+        data = csv.DictReader(csv_file)
+
+        for row in data:
+            print(row)
+
+
+main()
--- pset7-2-houses/roster.py
+++ pset7-2-houses/roster.py
@@ -1,1 +1,1 @@
-# TODO
+# A program that prints a list of students for a given house in alphabetical order
