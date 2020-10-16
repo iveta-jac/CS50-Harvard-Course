be92933ead856a1c5c98785585cba0f4c2e49cde
pocketful <iveta.jac@gmail.com>
2020-10-16 16:17:20 +0300

    full name word count, insert temporary table

--- pset7-2-houses/import.py
+++ pset7-2-houses/import.py
@@ -17,8 +17,22 @@ def main():
     with open(argv[1], 'r') as csv_file:
         data = csv.DictReader(csv_file)
 
+        # Iterate over students
         for row in data:
-            print(row)
+
+            # Separate each name into first, (middle), and last names
+            name = row["name"].split(" ")
+
+            # If there is no middle name
+            if len(name) == 2:
+                print("2")
+
+            # With middle name
+            if len(name) == 3:
+                print("3")
+
+            # Insert temporary data into the table
+            db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)", row["name"], "NULL", "NULL", row["house"], row["birth"])
 
 
 main()
